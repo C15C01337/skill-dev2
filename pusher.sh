03fa8c7035b3c5e596434d4d5c744fe82e36e8b5
Bishal Aryal <70318933+C15C01337@users.noreply.github.com>
2022-05-04 22:08:42 +0545

    Delete pusher.sh

--- pusher.sh
+++
@@ -1,8 +0,0 @@
-git init
-git config --global user.name "Bishal Aryal"
-git config --global user.email "easteregg@gmail.com"
-git add .
-git commit -m "$1"
-git branch -M main
-git remote add origin git@github.com:C15C01337/skill-dev2.git
-git push -u origin main -f 
